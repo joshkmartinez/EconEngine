f468e32c6cad585ce065cf07bb3a63b7b39f8edf
Joshua Martinez <joshkmartinez@gmail.com>
2021-02-14 00:26:36 -0800

    add opengraph image

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,11 @@ const MyApp = ({ Component, pageProps }) => {
       <Head>
         <title>EconEngine</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="EconEngine is a " />
+        <meta
+          name="description"
+          content="EconEngine is a search engine for financial assistance resources during the COVID-19 pandemic."
+        />
+        <meta property="og:image" content="/logo.png" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap"
