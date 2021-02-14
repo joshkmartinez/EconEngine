e46a2f5d8fee4c138422d45cf2badea2653060b6
Joshua Martinez <joshkmartinez@gmail.com>
2021-02-14 00:27:12 -0800

    improve searchbar

--- components/searchbar.js
+++ components/searchbar.js
@@ -1,19 +1,38 @@
-import { Box, Flex, Heading, Text, Link, Image } from "rebass";
+import { Box, Flex, Heading, Text, Link, Button } from "rebass";
 import { Label, Input } from "@rebass/forms";
+import dynamic from "next/dynamic";
+const Typewriter = dynamic(() => import("typewriter-effect"), {
+  ssr: false,
+});
+
+const examples = [
+  "Can I get rental assistance?",
+  "What are unemployment benefits?",
+  "What is mortgage relief?",
+  "How do I file for unemployment?",
+  "How can I open a bank account?",
+  "Is now a good time to buy a home?",
+];
 
 const searchbar = () => {
-  <Flex>
-    <Box>
-      <Label htmlFor="email">Email</Label>
-      <Input
-        id="email"
-        name="email"
-        type="email"
-        placeholder="jane@example.com"
+  return (
+    <Box width={[1, 4 / 5]} mx="auto">
+      <Flex>
+        <Box width={1}>
+          <Input placeholder="Search EconEngine!" />
+        </Box>
+        <Button mx={[1, 2]}  variant="primary">Search
+        </Button>
+      </Flex>
+      <Typewriter
+        options={{
+          strings: examples,
+          autoStart: true,
+          loop: true,
+        }}
       />
     </Box>
-    search
-  </Flex>;
+  );
 };
 
 export default searchbar;
